8658842dfd14855b50ed20fa4f3e02f502a7d444
Shane Hsi <shane@hsi.rocks>
2016-09-08 09:40:25 +0800

    尝试中 写一个 module

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -1,4 +1,4 @@
-if(!GULP_CONFIG.tasks.html) return
+if (!GULP_CONFIG.tasks.html) return
 
 var browserSync  = require("browser-sync")
 var data         = require("gulp-data")
@@ -12,30 +12,30 @@ var jade         = require("jade")
 var fs           = require("fs")
 
 var htmlTask = function() {
-
-	var exclude = path.normalize("!**/{" + GULP_CONFIG.tasks.html.excludeFolders.join(",") + "}/**")
-
+  
+  var exclude = path.normalize("!**/{" + GULP_CONFIG.tasks.html.excludeFolders.join(",") + "}/**");
+  
   var paths = {
-		src: [path.resolve(process.env.PWD, GULP_CONFIG.root.src, GULP_CONFIG.tasks.html.src, "**/*.{" + GULP_CONFIG.tasks.html.extensions + "}"), exclude],
+    src: [path.resolve(process.env.PWD, GULP_CONFIG.root.src, GULP_CONFIG.tasks.html.src, "**/*.{" + GULP_CONFIG.tasks.html.extensions + "}"), exclude],
     dest: path.resolve(process.env.PWD, GULP_CONFIG.root.dest, GULP_CONFIG.tasks.html.dest),
-  }
-
+  };
+  
   var getData = function(file) {
     var dataPath = path.resolve(process.env.PWD, GULP_CONFIG.root.src, GULP_CONFIG.tasks.html.src, GULP_CONFIG.tasks.html.dataFile)
-		return JSON.parse(fs.readFileSync(dataPath, "utf8"))
+    return JSON.parse(fs.readFileSync(dataPath, "utf8"))
   }
-
+  
   return gulp.src(paths.src)
     .pipe(data(getData))
-		.on("error", handleErrors)
-		.pipe(render({
-			pug: jade
-		}))
-		.on("error", handleErrors)
+    .on("error", handleErrors)
+    .pipe(render({
+      pug: jade
+    }))
+    .on("error", handleErrors)
     .pipe(gulpif(global.production, htmlmin(GULP_CONFIG.tasks.html.htmlmin)))
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
-
+  
 }
 
 gulp.task("html", htmlTask)
